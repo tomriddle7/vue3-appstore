c437e454668b6d515fe21bebc0d58998e3ae277d
tomriddle7 <tomriddle7@naver.com>
2020-12-10 23:27:48 +0900

    Add font awesome
    https://blinders.tistory.com/70?category=636275

---
+++ src/components/Loader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <font-awesome-icon icon="spinner" size="6x" pulse />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "loader",
+};
+</script>
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  display: -webkit-flex;
+  -webkit-align-item: center;
+  -webkit-justify-content: center;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import App from './App.vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faSpinner);
 
 const app = createApp(App);
 app.use(VueAxios, axios);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 
 app.mount('#app');
